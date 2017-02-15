03bdf6975a5305e39ce09aedd15f57e0793a2d1d
DESKTOP-52RPLJC\Javier Martinez <sparktechs@hotmail.com>
2017-02-15 15:53:40 -0800

    CTYSVBEL-31 / Se agregaron dependencias para la validacion de datos de login de administradores.

--- admin/login.php
+++ admin/login.php
@@ -7,6 +7,10 @@
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		
 		<link href="../css/main.css" rel="stylesheet">
+        
+        <script src="dist/sweetalert.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
